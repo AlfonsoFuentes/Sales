218e56a32a9892d7dd56e6b848fdd9a923b08d33
alfon <alfon@AFUENTES>
2024-06-02 17:51:58 -0500

    Add DBContext

---
+++ Sales/Infractusture/RegisterServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infractusture
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+          
+            return services;
+        }
+    }
+}
---
+++ Sales/Sales.Shared/Country.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Domain
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Filed {0} is required")]
+        [MaxLength(100, ErrorMessage ="Max length of {0} is less than {1} characters")]
+        [Display(Name = "Country")]
+        public string Name { get; set; } = string.Empty;
+
+
+    }
+}
